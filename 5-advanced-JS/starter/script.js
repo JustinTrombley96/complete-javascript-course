94b34cba2778567f9d46404657c0d9e7a1e763f8
Justin Trombley <jtrombley96@gmail.com>
2019-06-01 19:31:10 -0400

    Section 5 Lecture 100%

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -160,12 +160,133 @@
 
 //Lecture: IIFE  *******************************************************************
 
-(function() {
-	var score = Math.random() * 10;
-	console.log(score >= 5);
-})();
+// (function() {
+// 	var score = Math.random() * 10;
+// 	console.log(score >= 5);
+// })();
 
-(function(goodLuck) {
-	var score = Math.random() * 10;
-	console.log(score >= 5 - goodLuck);
-})();
+// (function(goodLuck) {
+// 	var score = Math.random() * 10;
+// 	console.log(score >= 5 - goodLuck);
+// })();
+
+//Lecture: Closures  *******************************************************************
+//An inner function always has access to the variables and parameters of its outer function, even after the outer function has returned.
+// function retirement(retirementAge) {
+// 	var a = ' years left until retirement.';
+
+// 	return function(yearOfBirth) {
+// 		var age = 2019 - yearOfBirth;
+// 		console.log(retirementAge - age + a);
+// 	};
+// }
+// //Option 1
+// var retirementUS = retirement(66);
+// retirementUS(1990);
+// //Option 2
+// // retirement(66)(1990);
+
+// var retirementGermany = retirement(65);
+// var retirementIceland = retirement(67);
+// retirementGermany(1990);
+// retirementIceland(1995);
+
+// function interviewQuestion(job) {
+// 	return function(name) {
+// 		if (job === 'designer') {
+// 			console.log(name + ', can you please explain UX design?');
+// 		} else if (job === 'teacher') {
+// 			console.log('What subject do you teach, ' + name + '?');
+// 		} else {
+// 			console.log('Hello ' + name + ', what do you do?');
+// 		}
+// 	};
+// }
+
+// interviewQuestion('teacher')('John');
+// interviewQuestion('designer')('Beth');
+// interviewQuestion('Writer')('Mikie');
+
+//Lecture: Bind, call and apply  *******************************************************************
+
+// var john = {
+// 	name         : 'John',
+// 	age          : 26,
+// 	job          : 'teacher',
+// 	presentation : function(style, timeOfDay) {
+// 		if (style === 'formal') {
+// 			console.log(
+// 				'Good ' +
+// 					timeOfDay +
+// 					' Ladies and gentlemen! Im ' +
+// 					this.name +
+// 					', Im a ' +
+// 					this.job +
+// 					' and Im ' +
+// 					this.age +
+// 					' years old. Have a nice ' +
+// 					timeOfDay,
+// 			);
+// 		} else if (style === 'friendly') {
+// 			console.log(
+// 				'Hey! Whats up? Im ' +
+// 					this.name +
+// 					', Im a ' +
+// 					this.job +
+// 					' and Im ' +
+// 					this.age +
+// 					' years old. Have a nice ' +
+// 					timeOfDay,
+// 			);
+// 		}
+// 	},
+// };
+
+// var emily = {
+// 	name : 'Emily',
+// 	age  : 35,
+// 	job  : 'designer',
+// };
+
+// john.presentation('formal', 'morning');
+
+// john.presentation.call(emily, 'friendly', 'afternoon');
+
+// // john.presentation.apply(emily, ['friendly', 'afternoon']);
+
+// var johnFriendly = john.presentation.bind(john, 'friendly');
+
+// johnFriendly('morning');
+// johnFriendly('night');
+
+// var emilyFormal = john.presentation.bind(emily, 'formal')
+// emilyFormal('afternoon')
+
+// var years = [ 1990, 1965, 1937, 2005, 1998 ];
+
+// function arrayCalc(arr, fn) {
+// 	var arrRes = [];
+// 	for (var i = 0; i < arr.length; i++) {
+// 		arrRes.push(fn(arr[i]));
+// 	}
+// 	return arrRes;
+// }
+
+// function calculateAge(el) {
+// 	return 2019 - el;
+// }
+
+// function isFullAge(limit, el) {
+// 	return el >= limit;
+// }
+
+// var ages = arrayCalc(years, calculateAge);
+
+// //Bind allows us to create a copy of an argument with a pre-set element
+
+// var fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
+// console.log(ages);
+// console.log(fullJapan);
+
+//Coding Challenge 7  *******************************************************************
+
